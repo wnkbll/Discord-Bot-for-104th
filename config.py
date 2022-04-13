4e3450d7a73b4bec134371c82beba547999e0485
wnkbll <91716343+wnkbll@users.noreply.github.com>
2022-04-13 16:42:20 +0500

    Обновленные файлы с комментариями

--- config.py
+++ config.py
@@ -1,10 +1,11 @@
 """ В 'token' записывается токен бота типа 'str'. В 'bot' записывается имя бота типа 'str'. В 'id' записывается id
-бота типа 'int'. В'prefix' записывается префикс  бота типа 'str'. В 'path' записывается путь к json-файлу в 
+бота типа 'int'. В'prefix' записывается префикс  бота типа 'str'. В 'path' записывается путь к json-файлу в
 формате 'str'. """
 settings = {
     'token': '',
     'bot': '',
     'id': '',
     'prefix': '//',
-    'path': 'data.json'
+    'path_old': 'data.json',
+    'path_total': 'data_total.json'
 }
